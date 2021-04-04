d9e53c6aa16d3fe56f7839916b1e3d2bcac357f3
EURosales <eduardo_rosales26@outlook.com>
2021-04-04 17:03:06 -0600

    countdown added, also, questions and its answers show correctly.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,37 @@
 const userName = document.getElementById('name');
 const startButton = document.getElementById('btnComenzar');
+const questionElement = document.getElementById('question');
+const anwserButtons = document.getElementById('answer-buttons');
+let name;
+
+const counter = document.getElementById('timer');
+
+function timer() {
+    const startingMinuts = 0.1;
+    let time = startingMinuts * 60;
+    let counterId = setInterval(() => {
+        updateCountdown();
+    }, 1000);
+
+    function updateCountdown() {
+        const minutes = Math.floor(time / 60);
+        let seconds = time % 60;
+
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+        counter.innerHTML = `Time: ${minutes}:${seconds}`;
+        time--;
+
+        if (time < 0) {
+            clearInterval(counterId);
+            //return 0;
+        }
+    }
+}
 
 //Trigger validators and get info
 startButton.addEventListener('click', (e) => {
     e.preventDefault();
-    let name = userName.value;
+    name = userName.value;
     userInputsValidator(name, isSelected());
 });
 
@@ -32,6 +59,7 @@ const registrationDiv = document.getElementById('registrationDiv');
 const body = document.getElementById('bg-body');
 const test = document.getElementsByClassName('content');
 const h1 = document.getElementsByClassName('name');
+let shuffledQuestions, currentQuestionIndex;
 
 function start() {
     mainRegistrationDiv.classList.add('hide');
@@ -51,13 +79,59 @@ function categoryLoader(categoryValue) {
 
 const genContainer = document.getElementById('gen-container');
 const spaceScreen = document.getElementById('space-question-container');
+const controller = document.getElementById('controller');
+
+/*
+anwserBTN.forEach(mish);
+function mish(element) {
+    element.addEventListener('click', (e) => {
+        controller.classList.remove('hide');
+    });
+}
+*/
+
 function startCoding() {
     body.classList.add('body-coding');
 }
 
 function startSpace() {
-    console.log('The space category function was reached.');
+    //console.log('The space category function was reached.');
+    //Se cambia la imagen de fondo
     body.classList.add('body-space');
+    //Se ocultan los primeros DIV's, lo que contienen la parte de registro.
     genContainer.classList.remove('hide');
     spaceScreen.classList.remove('hide');
+    //Reordena las preguntas en diferente orden
+    shuffledQuestions = spaceQuestions.sort(() => Math.random() - 0.5);
+    //Aqui se estable que comience por la posicion 0 del array que contiene las preguntas
+    currentQuestionIndex = 0;
+    //Una vez el orden de las preguntas esta sorteado, se comienzan a cargar en el div.
+    console.log(name);
+    setNextSpaceQuestion();
 }
+
+function setNextSpaceQuestion() {
+    timer();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+    /*Al h1 que contiene la pregunta, se le asigna la pregunta que este en la
+    posicion 0 despues de reordenar el arreglo que ccontiene las preguntas*/
+    //el primer "question es el parametro de la función en la que estamos actualmente."
+    //El segundo "question" es el nombre de la "Key" del diccionario que contiene la pregunta.
+    questionElement.innerText = question.question;
+    //Answers
+    question.answers.forEach((answer) => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('answer-btn');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+        anwserButtons.appendChild(button);
+    });
+}
+
+function selectAnswer(e) {}
